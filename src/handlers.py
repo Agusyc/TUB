6c8156fc19c5bb3d6b5dcb27e41281dc509083c6
Agustín Roth <agusrothdev@gmail.com>
2018-08-22 17:18:57 -0300

    Added some new commands

--- src/handlers.py
+++ src/handlers.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 from telethon import events
+import admin
+import re
 
 # Module-wide variables, because all the incoming and outgoing messages have to be able to read and modify this.
 afk = False
 afk_reason = ""
 
-def register(client):
+def register(client):    
+    # Handles any private messages
     @client.on(events.NewMessage(incoming=True))
-    async def incoming_all_handler(event):
-        print(event)
-    
-    @client.on(events.NewMessage(incoming=True, pattern=".*@AgEzRo.*"))
     async def incoming_handler(event):
-        if afk:
+        if afk and event.message.to_id.isinstanceof(PeerUser):
             await event.reply("Beep boop, I'm a bot. @AgEzRo is now AFK because: " + afk_reason)
 
-    @client.on(events.NewMessage(outgoing=True, pattern="^(?!Beep boop).*"))
+    # Handles any messages except those starting with "Beep boop",
+    # so that the bot doesn't trigger itself into turning off AFK mode
+    @client.on(events.NewMessage(outgoing=True, pattern="(?!^Beep boop).*"))
     async def outgoing_handler(event):
         global afk, afk_reason
         if afk:
@@ -30,7 +31,7 @@ def register(client):
 
     @client.on(events.NewMessage(outgoing=True, pattern="^!github$"))
     async def github_handler(event):
-        await event.message.edit("GitHub: https://github.com/Agusyc/")
+        await event.edit("GitHub: https://github.com/Agusyc/")
 
     @client.on(events.NewMessage(outgoing=True, pattern='^!source'))
     async def source_handler(event):
